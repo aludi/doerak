1b34a5525cb88ab1f80ed4e0091ea89b27445cc5
aludi <ludivanleeuwen@gmail.com>
2021-05-30 15:31:49 +0200

    final ppush

--- simple_doerak.py
+++ simple_doerak.py
@@ -11,6 +11,10 @@ class Number(Enum):
     TWO = 2
     THREE = 3
 
+class Knowledge(Enum):
+    K = "K"
+    C = "C"
+
 class Card:
     def __init__(self, suit, number):
         self.suit = suit
@@ -23,20 +27,39 @@ class Card:
     def set_owner(self, name):
         self.owner = name
 
+class KnowledgeOfPlayer:
+    def __init__(self, name):
+        self.name = name
+        self.first_order = []
+        self.second_order = []
+        ## ?? ##
+
+    def __repr__(self):
+        return "first order " + str(self.first_order)
+
+    def add_own_card(self, name, card):
+        k = (Knowledge.K, name, card, name)
+        self.first_order.append(k)
+
+
 
 class Player:
     def __init__(self, name, hand):
         self.name = name
-        self.knowledge = None
+        self.knowledge = KnowledgeOfPlayer(name)
         self.hand = hand
         self.update_card_owners()
 
     def __repr__(self):
-        return "\n Player " + str(self.name) + "\n " + str(self.hand)
+        return "\n Player " + str(self.name) + "\n " + str(self.hand) + "\n" + str(self.knowledge)
 
     def update_card_owners(self):
         for card in self.hand:
             card.set_owner(self.name)
+
+    def update_knowledge(self):
+        for card in self.hand:
+            self.knowledge.add_own_card(self.name, card)
 
 class Game:
     def __init__(self, number_of_players, hand_limit, player_names,
@@ -48,6 +71,7 @@ class Game:
         self.trump_card = 0
         self.trump_suit = 0
         self.turns_taken = 0
+
 
     def set_trump_card(self, card):
         self.trump_card = card
@@ -64,7 +88,7 @@ for suit in Suit:
 number_of_players = 3
 hand_limit = 1
 
-player_names = ["boris", "marieke", "ludi"]
+player_names = ["boris", "ludi", "marieke"]
 players = {}
 
 #random.shuffle(card_list)   # schudden
@@ -76,6 +100,7 @@ print(card_list)
 for player in player_names:
     hand = card_list[0:hand_limit]
     players[player] = Player(player, hand)
+    players[player].update_knowledge()
     del card_list[0:hand_limit]
 
 ## initialize pot ##
@@ -88,7 +113,18 @@ trump_card = players["pot"].hand[0]
 game.set_trump_card(trump_card)
 print(game.trump_suit)
 
-## implement fir
+### FIRST KNOWLEDGE POINT
+### NOt sure how we want to do this, i'll just put something
+## here for now ###
+
+# trump card is common knowledge
+# knowledge structured like a tuple? K_l Card
+
+
+
+
+## implement first player here...
+
 
 
 
